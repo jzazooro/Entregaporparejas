fd03023f6642ecdf48409ed064e169773e7f5ade
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-22 14:42:26 +0100

    subida

--- clases/palindromometododeinstancia.py
+++ clases/palindromometododeinstancia.py
@@ -1,1 +1,28 @@
+class palindromo:
+    global palabra
+    palabra=input("¿Que atributo o palabra desea añadir?: ")
+    global lista
+    lista=[]
 
+    def palabrapalindroma(palabra):
+
+        if palabra==palabra[: : -1]:
+            return True
+
+        elif palabra!=palabra[: : -1]:
+            return False
+
+    def test(palabra):
+        lista.append(palabra)
+
+        if palabra==palabra[: : -1]:
+            return True
+
+        elif palabra!=palabra[: : -1]:
+            return False
+
+        if len(lista)>1:
+            destruir=lista.pop()
+            destruir=destruir.upper()
+
+            print(destruir)
