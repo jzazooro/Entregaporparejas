f2880d8a174719e848b23d72885d9eaca1835864
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-03-22 09:27:13 +0100

    enunciado coppiado

--- clases/__pycache__/ejercicio_c.py
+++ clases/__pycache__/ejercicio_c.py
@@ -4,3 +4,14 @@ class A:
 
     def y(self,t):
         return len(t)
+
+a=A
+y= a.z
+print(y(a))
+aa=a()
+print(aa is a())
+z=aa.y
+print(z(()))
+print(a().y((a,)))
+print(A.y(aa (a,z)))
+print(aa.y((z,1,'z')))
