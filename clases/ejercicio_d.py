2f574c7799a06a7322b21bf4ce905fd766d9f1ce
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-03-22 14:21:16 +0100

    cambios en la clase Logger

--- clases/ejercicio_d.py
+++ clases/ejercicio_d.py
@@ -1,6 +1,9 @@
 class Logger:
-    def log(self,mensaje):
+    def log(self,mensaje,x):
         f= open('registry.txt','a+')
+        if (x==1):
+            f.write('--Start log--')
 
 class Test:
-    def llamada(self,mensaje,x):
+    def llamada(self,mensaje):
+        print()
