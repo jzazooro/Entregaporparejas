fde711f392283281051189bf6ec5f7297e2f8f67
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-03-22 14:28:43 +0100

    cambios en LOGGER

--- clases/ejercicio_d.py
+++ clases/ejercicio_d.py
@@ -1,11 +1,18 @@
+from encodings import utf_8
+
+
 class Logger:
     def log(self,mensaje,x):
         f= open('cat log.txt','a+')
         if (x==1):
             f.write('--Start log--'+'\n')
-        elif(x==5):
-            f.write('--End log:--'+'\n')
+        
+        
+        if(x==5):
+            f.write('--End log:--')
             f.write(str(x))
+            f.write('log(s)--')
+            f.close
 
 class Test:
     def llamada(self,mensaje):
